110cc40cefd2789d04ce40fb06c630d812a732ca
litswid <litswid95@gmail.com>
2025-05-20 22:28:15 +0300

    Работа над JSON и предзагрузчиком

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал корректно')
 });
+
+const preloader = document.querySelector(".preloader");
+    const content = document.querySelector(".content");
+    if (preloader && content) {
+        setTimeout(() => {
+            preloader.style.opacity = "0";
+            preloader.style.visibility = "hidden";
+            content.style.display = "block";
+            preloader.remove();
+        }, 3000);
+    }
